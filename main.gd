12d077f64a1665f0a8e7765145aed1daa633f574
jultty <juno.t@tuta.io>
2024-08-10 04:44:21 -0300

    Fix algorithm inversion

--- main.gd
+++ main.gd
@@ -3,9 +3,8 @@ extends Node2D
 @export var mob_scene: PackedScene
 
 func _on_easy_mob_timer_timeout():
-	print("Mob timeout")
 	var mob = mob_scene.instantiate()
-	var mob_spawn_location = $FirstMobSpawn/FirstMobSpawnLoc
+	var mob_spawn_location = $MobSpawnPath/MonSpawnPathFollower
 	mob_spawn_location.progress_ratio = randf()
-	
+	mob.position = mob_spawn_location.position
 	add_child(mob)
--- scripts/GoblinArea.gd
+++ scripts/GoblinArea.gd
@@ -2,17 +2,17 @@ extends Area2D
 
 @onready var _animation_player = $GoblinAnimationPlayer
 @onready var target = null
+@onready var ray = $GoblinRayCast
 
 var tile_size=16
 var direction = position.angle()
-var velocity = Vector2(randf_range(0.5, 1.5), 0.0)
-var chase_speed = 32
+var velocity = Vector2(randf_range(0.25, 0.75), 0.0)
+var chase_speed = randi_range(10, 18)
 var start_position = position
 var max_range = 200
 
 func _ready():
-	position = position.snapped(Vector2.ONE * tile_size)
-	position += Vector2.ONE * tile_size/2
+	ray.add_exception($GoblinArea/GoblinSightCollisionShape)
 	_animation_player.play("goblin-idle")
 
 func check_movement_range(pos):
@@ -26,21 +26,28 @@ func check_movement_range(pos):
 	else:
 		return true
 
+func move_if_not_colliding(new_position):
+	ray.target_position = new_position
+	ray.force_raycast_update()
+	#if !ray.is_colliding():
+	position = new_position
+
 func move(dir):
 	var signs = [ -1, 1 ]
 	var direction_sign = signs[randi() % signs.size()]
 	direction = randf_range(-PI / 4, PI / 4) * direction_sign
 	var new_position = position + velocity.rotated(dir) * direction_sign * tile_size	
+	ray.target_position = new_position
+	ray.force_raycast_update()
 	if check_movement_range(new_position):
-		position = new_position
+		move_if_not_colliding(new_position)
 	else:
 		print("[goblin]: Out of range", new_position)
 		move_to(start_position.x, start_position.y)
 
 func chase(area):
-	if area != self:
-		print("[goblin] Chasing: x", area.position.x, " y", area.position.y)
-		move_to(area.position.x, area.position.y)
+	print("[goblin] Chasing: x", area.position.x, " y", area.position.y)
+	move_to(area.position.x, area.position.y)
 
 func _on_timer_timeout():
 	if target == null:
@@ -59,27 +66,36 @@ func _on_goblin_sight_area_exited(area):
 		target = null
 
 func move_to(x, y):
-	#print("[goblin.move_to] Moving to: x", x, " y", y, " from x", self.position.x, " y", self.position.y)
-	var x_difference = x - self.position.x
-	var y_difference = y - self.position.y
-	#print("[goblin.move_to] Difference: x", x_difference, " y", y_difference)
+	var target_position = self.global_position
+	print("[goblin.move_to] Moving to: x", x, " y", y, " from x", self.global_position.x, " y", self.global_position.y)
+	var x_difference = self.global_position.x - x
+	var y_difference = self.global_position.y - y
+	print("[goblin.move_to] Difference: x", x_difference, " y", y_difference)
 	
 	if abs(x_difference) > chase_speed:
-		#print("[goblin.move_to] Target x over speed limit") 
-		if x_difference < 0:
-			#print("[goblin.move_to] Long x distance is negative, subtracting speed") 
-			self.position.x -= chase_speed
+		print("[goblin.move_to] Target x over speed limit") 
+		if x_difference > 0:
+			print("[goblin.move_to] Long x distance is negative, subtracting speed") 
+			self.global_position.x -= chase_speed
+			#target_position.x = self.global_position.x - chase_speed
 		else:
-			#print("[goblin.move_to] Long x distance is positive, adding speed") 
-			self.position.x += chase_speed
+			print("[goblin.move_to] Long x distance is positive, adding speed") 
+			self.global_position.x += chase_speed
+			#target_position.x = self.global_position.x + chase_speed
 	else:
-		#print("[goblin.move_to] Target x under speed limit, adding difference") 
-		self.position.x += x_difference
+		print("[goblin.move_to] Target x under speed limit, adding difference") 
+		self.global_position.x += x_difference
+		#target_position.x = self.global_position.x + x_difference
 
 	if abs(y_difference) > chase_speed:
-		if y_difference < 0:
-			self.position.y -= chase_speed
+		if y_difference > 0:
+			self.global_position.y -= chase_speed
+			#target_position.y = self.global_position.y - chase_speed
 		else:
-			self.position.y += chase_speed
+			self.global_position.y += chase_speed
+			#target_position.y = self.global_position.y + chase_speed
 	else:
-		self.position.y += y_difference
+		self.global_position.y += y_difference
+		#target_position.y = self.global_position.y + y_difference
+
+	#move_if_not_colliding(target_position)
